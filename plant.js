cd5b3a06c43a5c710ce065fc896a66f54d6452ba
Dino Beslagic <pp19dd@gmail.com>
2018-01-19 19:47:19 -0500

    Cleanup

--- plant.js
+++ plant.js
@@ -15,23 +15,15 @@ Plant.prototype = Object.create(Entity.prototype);
 Plant.prototype.onSprout = function() {
     this.Sprout.Stop();
     this.ShootingSolution.Start();
-
-    // this.disableSprout();
-    // this.enableShootingSolution();
 }
 
 Plant.prototype.onShootingSolution = function() {
-    // this.disableShootingSolution();
-    // this.enableShoot();
     this.ShootingSolution.Stop();
     this.Shoot.Start();
 }
 
 Plant.prototype.onShoot = function() {
     this.Shoot.Stop();
-    // this.Shoot.Reset();
-    // this.ShootingSolution.Reset();
-    // this.Sprout.Reset();
 
     if( this.public.life <= 0 ) {
         this.addCooldown( "Dying", { per_second: 1/5 } );
@@ -40,9 +32,4 @@ Plant.prototype.onShoot = function() {
     }
     this.Sprout.Start();
     this.public.life--;
-    // this.disableShoot();
-    // this.enableSprout();
-    // this.registerCooldowns([
-    //     { name: "derp_" + this.cooldowns.length, per_second: 3 }
-    // ]);
 }
