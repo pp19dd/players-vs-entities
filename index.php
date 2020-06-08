c4c75a2b4b5b84c2088890bdad563ffefcd9b07e
Dino Beslagic <pp19dd@gmail.com>
2020-06-08 10:47:44 -0400

    Move game to external js

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!doctype html>
 <html>
 <head>
-    <title>pve test</title>
+    <title>PvE test</title>
     <meta charset="utf-8" />
     <link rel="stylesheet" type="text/css" href="default.css?ts=<?php echo time() ?>" />
 </head>
@@ -15,62 +15,7 @@
 
     <script src="plant.js?ts=<?php echo time() ?>"></script>
     <script src="zombie.js?ts=<?php echo time() ?>"></script>
-    <script>
-        var engine = new Engine({ debug: true });
-        var renderer = new RendererOGA({
-            div: "display",
-            width:1000,
-            height:480
-        });
-        engine.setRenderer(renderer);
-        engine.setBoardSize(20, 8);
 
-        function make_terrain() {
-            for( var j = 0; j < engine.board_height; j++ ) {
-                for( var i = 0; i < engine.board_width; i++ ) {
-                    var t = "grass";
-                    if( i >= 2 && j >= 3 && j <= 4 ) t = "water";
-                    engine.setBoardXY(i, j, t);
-                }
-            }
-        }
-
-        function make_plant(x,y,life) {
-            var p = new Plant({ });
-            p.public.x = x;
-            p.public.y = y;
-            p.public.life = life;
-            engine.addEntity(p);
-        }
-
-        function make_zombie(x,y,life) {
-            var p = new Zombie({ });
-            p.public.x = x;
-            p.public.y = y;
-            p.public.life = life;
-            engine.addEntity(p);
-        }
-
-        // for( var i = 0; i < 20; i++ ) {
-        //     var x = parseInt(Math.random() * 10);
-        //     var y = parseInt(Math.random() * 8);
-        //     make_plant(x, y, 100);
-        // }
-
-        // for( var i = 0; i < 5; i++ ) {
-        //     var x = 5 + parseInt(Math.random() * 10);
-        //     var y = parseInt(Math.random() * 8);
-        //     make_zombie(x, y, 100);
-        // }
-
-        make_terrain();
-        make_plant(3,4,100);
-        make_zombie(15,4,100);
-
-        engine.Start();
-
-        //console.dir( engine.entities[0] );
-
-    </script>
+    <script src="game.js?ts=<?php echo time() ?>"></script>
 </body>
 </html>
